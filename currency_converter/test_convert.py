da9dc557abe36ef0a7741de7d491b6df5a29e7af
Damola Obaleke <45045727+damolaobaleke@users.noreply.github.com>
2023-07-27 15:31:10 -0700

    Update test_convert.py

--- currency_converter/test_convert.py
+++ currency_converter/test_convert.py
@@ -6,8 +6,7 @@ from app import app
 class currrency_convert_test(TestCase):
     def test_check_info(self):
         self.assertEqual(check_info("USD", "JPY", "500"), "ok")
-        self.assertEqual(check_info("USD", "ZZZ", "500"),
-                         "Invalid currency code")
+        self.assertEqual(check_info("USD", "ZZZ", "500"), "Invalid currency code")
         self.assertEqual(check_info("USD", "JPY", "asd"), "Invalid Amount")
 
     def test_convert_currency(self):
@@ -15,12 +14,11 @@ class currrency_convert_test(TestCase):
 
     def test_verify_form_info(self):
         with app.test_client() as client:
-            res = client.post(
-                "/verify_form", data={"from": "USD", "into": "JPY", "amount": "500"})
+            res = client.post("/verify_form", data={"from": "USD", "into": "JPY", "amount": "500"})
             self.assertEqual(res.status_code, 302)
             self.assertEqual(res.location, "/convert")
-            res = client.post(
-                "/verify_form", data={"from": "USD", "into": "ZZZ", "amount": "500"})
+            
+            res = client.post("/verify_form", data={"from": "USD", "into": "ZZZ", "amount": "500"})
             html = res.get_data(as_text=True)
             self.assertEqual(res.status_code, 200)
             self.assertIn(" <p>Invalid currency code</p>", html)
